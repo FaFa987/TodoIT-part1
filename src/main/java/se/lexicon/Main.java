46be6af2a27f3bfdb98e271472feab81bfb17e88
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-08 00:14:54 +0100

    TodoItem

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
         Person p1 = new Person(4, "Nisse" , "Olsson" ,  "nisse@gmail.com");
         System.out.println(p1.getSummary());
+        TodoItem creator1 = new TodoItem("drama" , "story composed in verse" , true, p1);
+        //TodoItem creator1 = new TodoItem("drama" , "story composed in verse" ,2024 , true, p1);
+        //System.out.println(creator1.);
+        System.out.println(creator1.getSummary());
 
     }
 }
