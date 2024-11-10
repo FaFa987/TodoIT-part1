e0690bfcbce988bd3275af6029dabd51760a0756
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-10 01:12:56 +0100

    done

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,9 +7,7 @@ public class Main {
 
         Person creator = new Person(4, "Nisse" , "Olsson" ,  "nisse@gmail.com");
         System.out.println(creator.getSummary());
-        //TodoItem creator1 = new TodoItem("drama" , "story composed in verse" , true, p1);
-        //TodoItem creator1 = new TodoItem("drama" , "story composed in verse" ,2024 , true, p1);
-        //System.out.println(creator.getSummary());
+
 
 
     }
